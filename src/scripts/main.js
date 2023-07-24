adc8930087b216c2de7b2441fc277cdee3880f59
Valeriia Yarzhemska <lera.strikalo@icloud.com>
2023-07-24 13:31:15 +0300

    Delete commented code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,38 +16,3 @@ window.addEventListener('hashchange', () => {
     document.body.style.overflow = 'auto';
   }
 });
-
-/* function setActiveNav() {
-  const navItems = document.querySelectorAll('.nav__item:not(:first-child)');
-  console.log(navItems);
-
-  navItems.forEach((item) => {
-    const sectionId = item.getAttribute('href');
-    const section = document.querySelector(sectionId);
-
-    const rect = section.getBoundingClientRect();
-    const isVisible = rect.top < window.innerHeight && rect.bottom >= 0;
-
-    if (isVisible) {
-      item.classList.add('nav__item--active');
-    } else {
-      item.classList.remove('nav__item--active');
-    }
-  });
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  setActiveNav();
-
-  window.addEventListener('scroll', setActiveNav);
-
-  document.querySelectorAll('.nav__item:not(:first-child)').forEach((item) => {
-    item.addEventListener('click', () => {
-      setTimeout(setActiveNav, 300);
-    });
-  });
-
-  window.addEventListener('hashchange', () => {
-    setActiveNav(); // Update active state on hashchange
-  });
-}); */
